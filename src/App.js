374fbc7f0d584a5dd689ec761a588a18bad5210b
aa1aac <ach.aayush.111@gmail.com>
2018-09-08 21:25:51 +0545

    hoc and other components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// checked 122
+import React, { Component } from 'react';
+import './index.css';
+import Layout from './components/layout/layout.js'
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+         <BurgerBuilder></BurgerBuilder>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App
