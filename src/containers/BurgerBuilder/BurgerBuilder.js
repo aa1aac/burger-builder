9524e271bacc059c0e62a807a08feb7406a3bd75
aa1aac <aayushacharya25@gmail.com>
2018-10-27 18:11:34 +0545

    add routing to the app and firebase backend support

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,39 +93,17 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        // this.setState( { loading: true } );
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: '',
-        //         address: {
-        //             street: '',
-        //             zipCode: '',
-        //             country: ''
-        //         },
-        //         email: ''
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post( '/orders.json', order )
-        //     .then( response => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } )
-        //     .catch( error => {
-        //         this.setState( { loading: false, purchasing: false } );
-        //     } );
-        const queryParms=[]
-        for (let i in this.state.ingredients){
-            queryParms.push(encodeURIComponent(i)+'='+encodeURIComponent(this.state.ingredients[i]))
+        
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        const queryString=queryParms.join('&')
-        this.props.history.push(
-            {
-                pathname:'checkout',
-                search:'?'+queryString,
-            }
-        );
+        queryParams.push('price=' + this.state.totalPrice);
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render () {
