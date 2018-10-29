9c2d2ae244e7c5c8001ed63e5fc949a6167ad9bb
aa1aac <aayushacharya25@gmail.com>
2018-10-29 10:34:15 +0545

    form validation control and minor bug fix

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
-import Backdrop from '../BackDrop/Backdrop';
+import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
